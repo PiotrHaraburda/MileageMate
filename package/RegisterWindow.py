69f6ff8e88e779b5cadc0c67e4b89ac2ddce5738
PiotrHaraburda <piotr.haraburda@student.wat.edu.pl>
2023-12-26 02:31:46 +0100

    - Defined Firebase connection - Account registration added

--- package/RegisterWindow.py
+++ package/RegisterWindow.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class RegisterWindow(ctk.CTk):
 
-    def __init__(self, master, other_window, mainImage, **kwargs):
+    def __init__(self, master, other_window, mainImage, crud, **kwargs):
         super().__init__(**kwargs)
         self.master = master
 
@@ -31,14 +31,14 @@ class RegisterWindow(ctk.CTk):
                                                border_spacing=0)
         self.firstNameTextBox.place(x=50, y=155)
 
-        self.secondNameLabel = ctk.CTkLabel(self.master, text="Second Name:", text_color="#555555",
-                                            font=("Trebuchet", 12, "bold"),
-                                            bg_color="white")
-        self.secondNameLabel.place(x=325, y=125)
-        self.secondNameTextBox = ctk.CTkTextbox(self.master, width=225, height=30, corner_radius=5, fg_color="#dbfde7",
-                                                bg_color="white", text_color="#555555", font=("Trebuchet", 12, "bold"),
-                                                border_spacing=0)
-        self.secondNameTextBox.place(x=325, y=155)
+        self.loginLabel = ctk.CTkLabel(self.master, text="Login:", text_color="#555555",
+                                       font=("Trebuchet", 12, "bold"),
+                                       bg_color="white")
+        self.loginLabel.place(x=325, y=125)
+        self.loginTextBox = ctk.CTkTextbox(self.master, width=225, height=30, corner_radius=5, fg_color="#dbfde7",
+                                           bg_color="white", text_color="#555555", font=("Trebuchet", 12, "bold"),
+                                           border_spacing=0)
+        self.loginTextBox.place(x=325, y=155)
 
         self.emailLabel = ctk.CTkLabel(self.master, text="Email:", text_color="#555555", font=("Trebuchet", 12, "bold"),
                                        bg_color="white")
@@ -54,22 +54,22 @@ class RegisterWindow(ctk.CTk):
         self.ageLabel.place(x=325, y=195)
         self.ageComboBox = ctk.CTkComboBox(self.master, width=225, height=30, corner_radius=5, fg_color="#dbfde7",
                                            bg_color="white", text_color="#555555", font=("Trebuchet", 12, "bold"),
-                                           border_width=0, button_color="#878787", values=["<18","18 to 25","26 to 35","36 to 50","51 to 60",">60"])
+                                           border_width=0, button_color="#878787",
+                                           values=["<18", "18 to 25", "26 to 35", "36 to 50", "51 to 60", ">60"])
         self.ageComboBox.place(x=325, y=225)
 
         self.passwordLabel = ctk.CTkLabel(self.master, text="Password:", text_color="#555555",
                                           font=("Trebuchet", 12, "bold"),
                                           bg_color="white")
         self.passwordLabel.place(x=50, y=265)
-        self.passwordTextBox = ctk.CTkTextbox(self.master, width=225, height=30, corner_radius=5, fg_color="#dbfde7",
-                                              bg_color="white", text_color="#555555", font=("Trebuchet", 12, "bold"),
-                                              border_spacing=0)
+        self.passwordTextBox = ctk.CTkEntry(self.master, width=225, height=30, corner_radius=5, fg_color="#dbfde7",
+                                              bg_color="white", text_color="#555555", font=("Trebuchet", 12, "bold"),border_width=0,show="*")
         self.passwordTextBox.place(x=50, y=295)
 
         self.registerButton = ctk.CTkButton(self.master, text="Register", fg_color="#146734",
                                             font=("Trebuchet", 14, "bold"),
                                             width=500, height=40, corner_radius=5, hover_color="#12552d",
-                                            bg_color="white")
+                                            bg_color="white", command=lambda: self.register_callback(crud))
         self.registerButton.place(x=50, y=375)
 
         self.alreadyRegisteredLabel = ctk.CTkLabel(self.master, text="Already registered?", text_color="#555555",
@@ -87,3 +87,12 @@ class RegisterWindow(ctk.CTk):
     def login_callback(self, other_window):
         self.master.withdraw()
         other_window.deiconify()
+
+    def register_callback(self, crud):
+        first_name = self.firstNameTextBox.get("0.0", 'end-1c')
+        login = self.loginTextBox.get("0.0", 'end-1c')
+        email = self.emailTextBox.get("0.0", 'end-1c')
+        age = self.ageComboBox.get()
+        password = self.passwordTextBox.get()
+
+        crud.createData(first_name, login, email, age, password)
